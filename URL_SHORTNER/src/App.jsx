997c815ad55048f018ace412fcbc34a3e37fa6b1
Anurag Shukla <anurag090697@gmail.com>
2024-09-16 02:06:58 +0530

    something

--- URL_SHORTNER/src/App.jsx
+++ URL_SHORTNER/src/App.jsx
@@ -3,10 +3,13 @@
 import { useState } from "react";
 import "./App.css";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import axios from "axios";
 
 function App() {
-  const [newUrl, setnewUrl] = useState("");
+  const [newUrl, setnewUrl] = useState("URL");
+  const [shorty, setShorty] = useState("SHORTENeD");
   const [errmsg, seterrmsg] = useState("");
+
   const justDoIt = () => {
     seterrmsg("Copied to clipboard....");
     setTimeout(() => {
@@ -14,17 +17,40 @@ function App() {
     }, 3000);
   };
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const response = await axios.post(
+      "https://url-shortner-api-5066.onrender.com/urlShortner",
+      {
+        url: newUrl,
+      }
+    );
+    // console.log(response);
+    let tm = response.data;
+    setShorty(tm.chotu);
+    setnewUrl(tm.url);
+    seterrmsg(tm.message);
+  }
+
   return (
     <div className='container'>
       <h1>URL SHORTNER</h1>
-      <form action=''>
-        <input type='text' />
+      <form action='' onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type='text'
+          value={newUrl}
+          onChange={(e) => setnewUrl(e.target.value)}
+        />
         <button>MODIFY URL</button>
       </form>
       <div>
         <CopyToClipboard text={newUrl} onCopy={justDoIt}>
-          <p className='ansp'>{newUrl}</p>
+          <p className='ansp'>URL</p>
         </CopyToClipboard>
+        <CopyToClipboard text={shorty} onCopy={justDoIt}>
+          <p className='ansp'>SHORTENED</p>
+        </CopyToClipboard>
+
         <p className='errmsg'>{errmsg}</p>
       </div>
     </div>
